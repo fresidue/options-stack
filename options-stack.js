5cb01f33d4290e26be7a7db013437be9bc4a328c
fresidue <fresidue@gmail.com>
2018-06-04 19:25:54 +0200

    a config stack that allows for multiple config subtypes with multiple default levels

---
+++ options-stack.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const _ = require('lodash');
+const assign = _.assign;
+const reduce = _.reduce;
+
+// const assign = Object.assign;
+// const reduce = Array.reduce;
+
+const createStack = function (rootOpts, rootFilter) {
+  // the stateful options storage
+  const stack = [];
+  // adding options - filter can be null (will apply to ALL filters)
+  const addOptions = (options, filter) => {
+    stack.push({
+      filter,
+      options: assign({}, options)
+    });
+    console.log('stack = ', stack);
+  };
+  addOptions(rootOpts, rootFilter);
+
+  const getOptions = (filter, transientOpts) => {
+    return reduce(stack, (acc ,layer) => {
+      if (!layer.filter || filter === layer.filter) {
+        return assign(acc, layer.options);
+      } else {
+        return acc;
+      }
+    }, assign({}, transientOpts));
+  };
+
+  return {
+    stack,
+    options: getOptions,
+    addOptions,
+  };
+};
+
+module.exports = createStack;
---
+++ test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const _ = require('lodash');
+const assert = require('assert');
+
+const createStack = require('./options-stack');
+
+describe('testing stack', () => {
+  it('simple case', () => {
+    const stack = createStack({
+      a: 'a', b: 'b', c: 'c'
+    });
+    stack.addOptions({
+      b: 'bb', c: undefined, d: 'd'
+    });
+    const opts = stack.options();
+    assert(_.isEqual(opts, {
+      a: 'a', b: 'bb', c: undefined, d:'d'
+    }));
+  });
+
+  it('with filter', () => {
+    const stack = createStack({
+      a: 'a', b: 'b', c: 'c'
+    });
+    stack.addOptions({
+      a: 'dog'
+    }, 'dog');
+    stack.addOptions({
+      b: 'cat',
+      c: 'cat'
+    }, 'cat');
+    stack.addOptions({
+      c: 'mouse'
+    }, 'cat');
+    const opts = stack.options();
+    const dogOpts = stack.options('dog');
+    assert(_.isEqual(dogOpts, {
+      a: 'dog', b: 'b', c: 'c'
+    }));
+    const catOpts = stack.options('cat');
+    assert(_.isEqual(catOpts, {
+      a: 'a', b: 'cat', c: 'mouse'
+    }));
+    const mouseOpts = stack.options('mouse');
+    assert(_.isEqual(mouseOpts, {
+      a: 'a', b: 'b', c: 'c'
+    }));
+  });
+});
