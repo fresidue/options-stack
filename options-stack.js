838826163d08a2f3d4d886d8baaa61b02d282289
fresidue <fresidue@gmail.com>
2018-06-21 23:42:49 +0200

    polish

--- options-stack.js
+++ options-stack.js
@@ -1,12 +1,5 @@
 'use strict';
 
-const _ = require('lodash');
-const assign = _.assign;
-const reduce = _.reduce;
-
-// const assign = Object.assign;
-// const reduce = Array.reduce;
-
 const createStack = function (rootOpts, rootFilter) {
   // the stateful options storage
   const stack = [];
@@ -14,20 +7,20 @@ const createStack = function (rootOpts, rootFilter) {
   const addOptions = (options, filter) => {
     stack.push({
       filter,
-      options: assign({}, options)
+      options: Object.assign({}, options)
     });
     // console.log('stack = ', stack);
   };
   addOptions(rootOpts, rootFilter);
 
   const getOptions = (filter, transientOpts) => {
-    return reduce(stack, (acc ,layer) => {
+    return stack.reduce((acc, layer) => {
       if (!layer.filter || filter === layer.filter) {
-        return assign(acc, layer.options);
+        return Object.assign(acc, layer.options);
       } else {
         return acc;
       }
-    }, assign({}, transientOpts));
+    }, Object.assign({}, transientOpts));
   };
 
   return {
